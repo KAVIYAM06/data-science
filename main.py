7b9614f9252290a6f7f67796daa4c416d9981ca8
KAVIYAM06 <mkaviyamkaviya2004@gmail.com>
2025-07-14 13:33:36 +0530

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from controllers.linkedin_controller import router
-
-app = FastAPI(title="LinkedIn Stealth Automation API")
-app.include_router(router, prefix="/linkedin")
